c573f79c31a6448a5828a7ca888bd5b0f78a7303
ATikh69 <79520643490@ya.ru>
2022-07-13 15:18:25 +0300

    fix

--- src/Square.py
+++ src/Square.py
@@ -3,11 +3,3 @@ from src.Rectangle import Rectangle
 
 class Square(Rectangle):
     NAME = "SQUARE"
-
-    @property
-    def area(self):
-        return self.a * self.b
-
-    @property
-    def perimeter(self):
-        return self.a * 2 + self.b * 2
